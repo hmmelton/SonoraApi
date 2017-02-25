3e73441ea8d7d9809e25fa36c15181df38bc18e1
Harrison Melton <hmmelton@comcast.net>
2017-02-25 00:42:22 -0800

    renamed directory

--- Sonora/Constants.go
+++
@@ -1,9 +0,0 @@
-package main
-
-const (
-	// Mongo DB constants
-	MongoDBHosts = "ds149069.mlab.com:49069"
-	AuthDatabase = "sonora"
-	AuthUserName = "hmmelton"
-	AuthPassword = "Lampard#8"
-)
---
+++ src/sonora/TableStructs.go
@@ -0,0 +1,37 @@
+package main
+
+type (
+	Ingredient struct {
+		Name		string	`json:"name"`
+		Count		float32	`json:"count"`
+		Count_Type	string	`json:"count_type"`
+	}
+
+	Recipe struct {
+		Id				string			`json:"id"`
+		User_Id			string			`json:"user_id"`
+		First_Name		string			`json:"first_name"`
+		Last_Name		string			`json:"last_name"`
+		Name			string			`json:"name"`
+		Image			string			`json:"image"`
+		Instructions	[]string		`json:"instructions"`
+		Ingredients		[]Ingredient	`json:"ingredients"`
+		Tags			[]string		`json:"tags"`
+		Featured		bool			`json:"featured"`
+	}
+
+	Menu struct {
+		Id			string		`json:"id"`
+		UserId		string		`json:"user_id"`
+		Image		string		`json:"image"`
+		ProfImage	string		`json:"prof_image"`
+		Name		string		`json:"name"`
+		Appetizers	*[]Recipe	`json:"appetizers"`
+		Entrees		*[]Recipe	`json:"entrees"`
+		Sides		*[]Recipe	`json:"sides"`
+		Desserts	*[]Recipe	`json:"desserts"`
+		Beverages	*[]Recipe	`json:"beverages"`
+		Tags		[]string	`json:"tags"`
+		Featured	bool		`json:"featured"`
+	}
+)
