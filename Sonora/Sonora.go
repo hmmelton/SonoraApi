c68a4f20acac547858a8e9b28a17890f79bc1766
Harrison Melton <hmmelton@comcast.net>
2017-02-25 00:35:23 -0800

    moved everything inside a new folder

---
+++ Sonora/Sonora.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	SetUpMongo()
+	// Create router
+	router := mux.NewRouter()
+
+	// Recipe endpoints
+	router.HandleFunc("/recipe", GetRecipeEndpoint).Methods("GET")
+	router.HandleFunc("/recipe", PostRecipeEndpoint).Methods("POST")
+	router.HandleFunc("/recipe", DeleteRecipeEndpoint).Methods("DELETE")
+	router.HandleFunc("/recipe", UpdateRecipeEndpoint).Methods("PUT")
+	// Menu endpoints
+	router.HandleFunc("/menu", GetMenuEndpoint).Methods("GET")
+	router.HandleFunc("/menu", PostMenuEndpoint).Methods("POST")
+	router.HandleFunc("/menu", DeleteMenuEndpoint).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":5000", router))
+}
